e65194098908181d3e7c803b5ab5c1bc365cb9df
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-10-22 01:50:01 +0700

    До замены строк

--- Scaner.cpp
+++ Scaner.cpp
@@ -42,247 +42,240 @@ void Scaner::PrintError(const char* error, const char* text)
 
 int Scaner::UseScaner(type_lex lex)
 {
-	int i = 0;
-	lex[0] = '\0';
-start:
-	while (text[uk] == ' ' || text[uk] == '\t' || text[uk] == '\n')
-		uk++;
-	i = 0;
+    int i = 0;
+    lex[0] = '\0';
 
-	// Конец программы 
-	if (text[uk] == '\0')
-	{
-		lex[i++] = '#';
-		lex[i] = '\0';
-		return typeEnd;
-	}
+    while (true) 
+    {
+        // Пропускаем пробелы
+        while (text[uk] == ' ' || text[uk] == '\t' || text[uk] == '\n') 
+        {
+            uk++;
+        }
 
-	if (text[uk] == '/')
-	{
-		uk++;
-		// Комментарии
-		if (text[uk] == '/')
-		{
-			std::cout << "Строка комментария." << std::endl;
-			uk++;
-			while (text[uk] != '\n' && text[uk] != '\0')
-				uk++;
-			goto start;
-		}
-		else
-		{
-			// Деление
-			uk++;
-			lex[i++] = '/';
-			lex[i++] = '\0';
-			return typeDiv;
-		}
-	}
+        // Конец программы 
+        if (text[uk] == '\0') 
+        {
+            lex[i] = '\0';
+            return typeEnd;
+        }
 
-	if (isdigit(text[uk])) {
-		while (isdigit(text[uk]) && i < maxNumber - 1)
-		{
-			lex[i++] = text[uk++];
-		}
+        // Обработка комментариев
+        if (text[uk] == '/') 
+        {
+            uk++;
+            if (text[uk] == '/') 
+            {
+                std::cout << "Строка комментария." << std::endl;
+                while (text[uk] != '\n' && text[uk] != '\0') 
+                {
+                    uk++;
+                }
+                continue;  
+            }
+            else 
+            {
+                // Деление
+                lex[i++] = '/';
+                lex[i++] = '\0';
+                return typeDiv;
+            }
+        }
 
-		// Обработка вещественных чисел
-		if (text[uk] == '.') {
-			lex[i++] = text[uk++];
-			while (isdigit(text[uk]) && i < maxNumber - 1) {
-				lex[i++] = text[uk++];
-			}
-			lex[i] = '\0';
-			if (i == maxNumber - 1 && isdigit(text[uk]))
-			{
-				while (isdigit(text[uk]))
-					uk++;
-				PrintError("Константа превышает максимальную длину лексемы: ", lex);
-				return typeError;
-			}
-			return typeFloat; 
-		}
+        // Обработка чисел
+        if (isdigit(text[uk])) 
+        {
+            while (isdigit(text[uk]) && i < maxNumber - 1) 
+            {
+                lex[i++] = text[uk++];
+            }
 
-		lex[i] = '\0';
-		return typeInt; 
-	}
+            // Обработка вещественных чисел
+            if (text[uk] == '.') {
+                lex[i++] = text[uk++];
+                while (isdigit(text[uk]) && i < maxNumber - 1) 
+                {
+                    lex[i++] = text[uk++];
+                }
+                lex[i] = '\0';
+                if (i == maxNumber - 1 && isdigit(text[uk]))
+                {
+                    while (isdigit(text[uk])) 
+                    {
+                        uk++;
+                    }
+                    PrintError("Константа превышает максимальную длину лексемы: ", lex);
+                    return typeError;
+                }
+                return typeFloat;
+            }
 
-	// Обработка числа, начинающегося с точки
-	if (text[uk] == '.')
-	{
-		lex[i++] = text[uk++];
-		if (isdigit(text[uk])) {
-			while (isdigit(text[uk]) && i < maxNumber - 1) {
-				lex[i++] = text[uk++];
-			}
-			lex[i] = '\0';
-			if (i == maxNumber - 1 && isdigit(text[uk]))
-			{
-				while (isdigit(text[uk]))
-					uk++;
-				PrintError("Константа превышает максимальную длину лексемы: ", lex);
-				return typeError;
-			}
-			return typeFloat; 
-		}
-		else
-		{
-			return typePoint;
-		}
-	}
+            lex[i] = '\0';
+            return typeInt;
+        }
 
-	// Идентификаторы
-	if (isalpha(text[uk]) || text[uk] == '_')
-	{
-		while ((isalnum(text[uk]) || text[uk] == '_') && i < maxLex - 1)
-		{ 
-			lex[i++] = text[uk++];
-		}
-		lex[i] = '\0';
-		if (i == maxLex - 1 && (isalnum(text[uk]) || text[uk] == '_'))
-		{
-			while (isalnum(text[uk]) || text[uk] == '_')
-				uk++;
-			PrintError("Идентификатор превышает максимальную длину лексемы.", lex);
-			return typeError;
-		}
+        // Обработка точки
+        if (text[uk] == '.') 
+        {
+            lex[i++] = text[uk++];
+            if (isdigit(text[uk])) 
+            {
+                while (isdigit(text[uk]) && i < maxNumber - 1) 
+                {
+                    lex[i++] = text[uk++];
+                }
+                lex[i] = '\0';
+                if (i == maxNumber - 1 && isdigit(text[uk])) 
+                {
+                    while (isdigit(text[uk])) 
+                    {
+                        uk++;
+                    }
+                    PrintError("Константа превышает максимальную длину лексемы: ", lex);
+                    return typeError;
+                }
+                return typeFloat;
+            }
+            else 
+            {
+                return typePoint;
+            }
+        }
 
-		for (int j = 0; j < maxKeyword; j++)
-		{
-			if (strcmp(lex, keyword[j]) == 0)
-			{
-				return indexKeyword[j];
-			}
-		}
-		return typeId;
-	}
+        // Обработка идентификаторов
+        if (isalpha(text[uk]) || text[uk] == '_') {
+            while ((isalnum(text[uk]) || text[uk] == '_') && i < maxLex - 1) 
+            {
+                lex[i++] = text[uk++];
+            }
+            lex[i] = '\0';
+            if (i == maxLex - 1 && (isalnum(text[uk]) || text[uk] == '_')) 
+            {
+                while (isalnum(text[uk]) || text[uk] == '_') 
+                {
+                    uk++;
+                }
+                PrintError("Идентификатор превышает максимальную длину лексемы.", lex);
+                return typeError;
+            }
 
-	switch (text[uk])
-	{
-	case ',': 
-		uk++; lex[i++] = ','; 
-		lex[i] = '\0'; 
-		return typeComma;
-	case ';': 
-		uk++; lex[i++] = ';'; 
-		lex[i] = '\0'; 
-		return typeSemicolon;
-	case '(': uk++; 
-		lex[i++] = '('; 
-		lex[i] = '\0'; 
-		return typeLeftBracket;
-	case ')': uk++; 
-		lex[i++] = ')'; 
-		lex[i] = '\0'; 
-		return typeRightBracket;
-	case '{': uk++; 
-		lex[i++] = '{';
-		lex[i] = '\0'; 
-		return typeLeftBrace;
-	case '}': 
-		uk++; lex[i++] = '}'; 
-		lex[i] = '\0'; 
-		return typeRightBrace;
-	case '+': 
-		uk++; 
-		lex[i++] = '+'; 
-		lex[i] = '\0'; return typePlus;
-	case '-': 
-		uk++; 
-		lex[i++] = '-'; 
-		lex[i] = '\0'; 
-		return typeMinus;
-	case '*': 
-		uk++; lex[i++] = '*'; 
-		lex[i] = '\0'; 
-		return typeMul;
-	case '%': 
-		uk++; 
-		lex[i++] = '%'; 
-		lex[i] = '\0'; 
-		return typeMod;
-	case '=':
-		uk++;
-		lex[i++] = '=';
-		if (text[uk] == '=')
-		{
-			uk++;
-			lex[i++] = '=';
-			lex[i] = '\0';
-			return typeEq;
-		}
-		else
-		{
-			lex[i] = '\0';
-			return typeEval;
-		}
-	case '!':
-		uk++;
-		lex[i++] = '!';
-		if (text[uk] == '=')
-		{
-			uk++;
-			lex[i++] = '=';
-			lex[i] = '\0';
-			return typeUnEq;
-		}
-		else
-		{
-			lex[i] = '\0';
-			PrintError("Неожидаемый символ: ", lex);
-			return typeError;
-		}
-	case '>':
-		uk++;
-		lex[i++] = '>';
-		if (text[uk] == '=')
-		{
-			uk++;
-			lex[i++] = '=';
-			lex[i] = '\0';
-			return typeMoreOrEq;
-		}
-		else if (text[uk] == '>')
-		{
-			uk++;
-			lex[i++] = '>';
-			lex[i] = '\0';
-			return typeBitwiseRight;
-		}
-		else
-		{
-			lex[i] = '\0';
-			return typeMore;
-		}
-	case '<':
-		uk++;
-		lex[i++] = '<';
-		if (text[uk] == '=')
-		{
-			uk++;
-			lex[i++] = '=';
-			lex[i] = '\0';
-			return typeLessOrEq;
-		}
-		else if (text[uk] == '<')
-		{
-			uk++;
-			lex[i++] = '<';
-			lex[i] = '\0';
-			return typeBitwiseLeft;
-		}
-		else
-		{
-			lex[i] = '\0';
-			return typeMore;
-		}
-	default:
-		lex[i++] = text[uk];
-		lex[i] = '\0';
-		PrintError("Лексическая ошибка в строке ", lex);
-		uk++;
-		return typeError;
-	}
+            // Проверяем, является ли идентификатор ключевым словом
+            for (int j = 0; j < maxKeyword; j++) 
+            {
+                if (strcmp(lex, keyword[j]) == 0) 
+                {
+                    return indexKeyword[j];
+                }
+            }
+            return typeId;
+        }
+
+        // Обработка специальных символов
+        switch (text[uk]) 
+        {
+        case ',':
+            uk++; lex[i++] = ','; lex[i] = '\0'; return typeComma;
+        case ';':
+            uk++; lex[i++] = ';'; lex[i] = '\0'; return typeSemicolon;
+        case '(':
+            uk++; lex[i++] = '('; lex[i] = '\0'; return typeLeftBracket;
+        case ')':
+            uk++; lex[i++] = ')'; lex[i] = '\0'; return typeRightBracket;
+        case '{':
+            uk++; lex[i++] = '{'; lex[i] = '\0'; return typeLeftBrace;
+        case '}':
+            uk++; lex[i++] = '}'; lex[i] = '\0'; return typeRightBrace;
+        case '+':
+            uk++; lex[i++] = '+'; lex[i] = '\0'; return typePlus;
+        case '-':
+            uk++; lex[i++] = '-'; lex[i] = '\0'; return typeMinus;
+        case '*':
+            uk++; lex[i++] = '*'; lex[i] = '\0'; return typeMul;
+        case '%':
+            uk++; lex[i++] = '%'; lex[i] = '\0'; return typeMod;
+        case '=':
+            uk++;
+            lex[i++] = '=';
+            if (text[uk] == '=') 
+            {
+                uk++;
+                lex[i++] = '=';
+                lex[i] = '\0';
+                return typeEq;
+            }
+            else 
+            {
+                lex[i] = '\0';
+                return typeEval;
+            }
+        case '!':
+            uk++;
+            lex[i++] = '!';
+            if (text[uk] == '=') 
+            {
+                uk++;
+                lex[i++] = '=';
+                lex[i] = '\0';
+                return typeUnEq;
+            }
+            else 
+            {
+                lex[i] = '\0';
+                PrintError("Неожидаемый символ: ", lex);
+                return typeError;
+            }
+        case '>':
+            uk++;
+            lex[i++] = '>';
+            if (text[uk] == '=') 
+            {
+                uk++;
+                lex[i++] = '=';
+                lex[i] = '\0';
+                return typeMoreOrEq;
+            }
+            else if (text[uk] == '>') 
+            {
+                uk++;
+                lex[i++] = '>';
+                lex[i] = '\0';
+                return typeBitwiseRight;
+            }
+            else {
+                lex[i] = '\0';
+                return typeMore;
+            }
+        case '<':
+            uk++;
+            lex[i++] = '<';
+            if (text[uk] == '=') 
+            {
+                uk++;
+                lex[i++] = '=';
+                lex[i] = '\0';
+                return typeLessOrEq;
+            }
+            else if (text[uk] == '<') 
+            {
+                uk++;
+                lex[i++] = '<';
+                lex[i] = '\0';
+                return typeBitwiseLeft;
+            }
+            else 
+            {
+                lex[i] = '\0';
+                return typeLess;
+            }
+        default:
+            lex[i++] = text[uk++];
+            lex[i] = '\0';
+            PrintError("Лексическая ошибка в строке ", lex);
+            return typeError;
+        }
+    }
 }
+
 
 void Scaner::GetData(const char* filename)
 {
